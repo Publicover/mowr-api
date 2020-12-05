ed4b90b67e0de60926385ec3ec6a9f584b33055a
Jim Publicover <jameslpublicover@gmail.com>
2020-12-05 16:37:12 +0100

    tests, user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,22 +1,51 @@
 class Api::V1::UsersController < ApplicationController
+  # before_action :set_user, except: [:index, :show]
+
   def index
+    if current_user.admin?
+      @users = User.all
+      json_response(@users)
+    else
+      json_response({ message: Message.invalid_credentials})
+    end
   end
 
   def show
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
-  def create
+    if current_user.admin?
+      @user = User.find(params[:target_id])
+    else
+      @user = User.find(params[:id])
+    end
+    json_response(@user)
   end
 
   def update
+    if current_user.admin?
+      @user = User.find(params[:target_id])
+    else
+      @user = User.find(params[:id])
+    end
+    @user.update(user_params)
+    json_response(@user)
   end
 
   def destroy
+    if current_user.admin?
+      @user = User.find(params[:target_id])
+    else
+      @user = User.find(params[:id])
+    end
+    @user.destroy
+    json_response(@user)
   end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:email, :f_name, :l_name, :password, :password_confirmation, :role, :target_id)
+    end
 end
--- test/controllers/authentication_controller_test.rb
+++ test/controllers/authentication_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class AuthenticationControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  require 'test_helper'
+
+  class AuthenticationControllerTest < ActionDispatch::IntegrationTest
+    setup do
+      @user = User.create(email: 'jim@home.com', f_name: 'Jim', l_name: 'Pub', password: 'password', password_confirmation: 'password', role: :admin)
+      @headers = valid_headers(@user.id).except('Authorization')
+      @valid_creds = { email: @user.email, password: @user.password }.to_json
+      @invalid_creds = { email: Faker::Internet.email, password: Faker::Internet.password }.to_json
+    end
+
+    test 'when request is valid' do
+      post auth_login_path, headers: @headers, params: @valid_creds
+      assert_not_nil json['auth_token']
+    end
+
+    test 'when request is invalid' do
+      post auth_login_path, headers: @headers, params: @invalid_creds
+      assert_match json['message'], Message.invalid_credentials
+    end
+  end
+
 end
