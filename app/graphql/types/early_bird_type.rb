e52728632ba2143e9d8dffedd53bc8b86d4dd46a
Jim Publicover <jameslpublicover@gmail.com>
2021-01-05 18:01:04 +0100

    whoops, nope, now i have all the queries

--- app/graphql/types/early_bird_type.rb
+++ app/graphql/types/early_bird_type.rb
@@ -4,7 +4,8 @@ module Types
   class EarlyBirdType < Types::BaseObject
     def self.authorized?(object, context)
       super && (context[:current_user].admin? ||
-                context[:current_user].driver?)
+                context[:current_user].driver? ||
+                object.user == context[:current_user])
     end
     field :id, ID, null: false
     field :priority, Integer, null: false
---
+++ test/graphql/queries/customer/early_bird_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class Queries::EarlyBirdTest < ActionDispatch::IntegrationTest
+  test 'should not get early birds as customer' do
+    graphql_as_customer
+
+    post graphql_path, params: { query: fetch_early_birds_helper }
+
+    assert_response :success
+    assert_equal Message.unauthorized, json['errors'][0]['message']
+  end
+
+  test 'should only get own early bird as customer' do
+    graphql_as_customer
+
+    post graphql_path, params: { query: fetch_early_bird_helper(early_birds(:one).id) }
+
+    assert_response :success
+    assert_equal Message.unauthorized, json['errors'][0]['message']
+
+    post graphql_path, params: { query: fetch_early_bird_helper(early_birds(:two).id) }
+
+    assert_response :success
+    assert_equal early_birds(:two).id, json['data']['fetchEarlyBird']['id'].to_i
+  end
+end
