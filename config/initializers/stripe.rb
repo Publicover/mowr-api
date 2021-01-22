1c9cd357d4e771c6221b90bd72fe1d36004251e8
Jim Publicover <jameslpublicover@gmail.com>
2021-01-22 14:00:04 +0100

    wait for ActiveRecord to load during initialization

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-Stripe.api_key = ENV['PLOWR_STRIPE_SK']
-StripeEvent.signing_secret = ENV['PLOWR_STRIPE_WEBHOOK']
+ActiveSupport.on_load(:active_record) do
+  Stripe.api_key = ENV['PLOWR_STRIPE_SK']
+  StripeEvent.signing_secret = ENV['PLOWR_STRIPE_WEBHOOK']
 
-StripeEvent.configure do |events|
-  events.subscribe 'charge.succeeded', Stripe::ChargeSucceeded.new
+  StripeEvent.configure do |events|
+    events.subscribe 'charge.succeeded', Stripe::ChargeSucceeded.new
+  end
 end
